00a7abe0d25fc7f17ad949126c6df02d6c8a2499
Christopher Mühl <christopher@dieschittigs.de>
2018-11-23 14:26:37 +0100

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor (args, opts) {
+        super(args, opts);
+
+        this.option('babel');
+    }
+};
+
