4680a17eb9740b8c22d551fae25538cc1ee2c6fa
Christopher Mühl <christopher@dieschittigs.de>
2018-11-28 10:33:45 +0100

    Generate composer.json

--- app/index.js
+++ app/index.js
@@ -5,7 +5,39 @@ module.exports = class extends Generator {
     constructor (args, opts) {
         super(args, opts);
 
-        this.option('babel');
+        this.option('namespace');
+    }
+
+    async prompting() {
+        this.answers = await this.prompt([{
+            type    : 'input',
+            name    : 'composername',
+            message : 'Name for your composer project',
+            default : 'example/bundle',
+        }, {
+            type    : 'input',
+            name    : 'description',
+            message : 'Would you like to add a description?',
+        }, {
+            type    : 'input',
+            name    : 'namespace',
+            message : 'PSR-4 namespace',
+            default : 'Example\\Bundle',
+        }]);
+
+        this.answers.namespace = this.answers.namespace.replace(/\\+$/, ''); // Trim trailing backslashes 
+    }
+
+    copy () {
+        let context = this.answers;
+        context.namespace = context.namespace.replace(/\\/g, '\\\\');
+        context.description = context.description ? `\n    "description": "${context.description}",` : '';
+
+        this.fs.copyTpl(
+            this.templatePath('composer.json'),
+            this.destinationPath('composer.json'),
+            context
+        );
     }
 };
 
